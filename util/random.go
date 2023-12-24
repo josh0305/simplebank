ba38ee89d2ced832d0890dcfdc3f5cf00b8e5f85
Joshua Huang <joshua@Joshuade-MacBook-Pro.local>
2023-12-23 22:47:19 -0500

    generate sql for user table, write unit test for sql, implement hash password

--- util/random.go
+++ util/random.go
@@ -1,6 +1,7 @@
 package util
 
 import (
+	"fmt"
 	"math/rand"
 	"strings"
 	"time"
@@ -42,4 +43,8 @@ func RandomCurrency() string {
 	currencies := []string{EUR, CAD, USD}
 	k := len(currencies)
 	return currencies[randomObj.Intn(k)]
+}
+
+func RandomEmail() string {
+	return fmt.Sprintf("%s@email.com", RandomString(6))
 }
